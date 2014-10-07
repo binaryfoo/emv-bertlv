b0526cf1590ef2a1a8588900f1e1dadf7cd2a988
Will Currie <will@currie.id.au>
2014-10-07 18:51:40 +1100

    Recover signed static data experiment.

--- src/main/java/io/github/binaryfoo/decoders/SignedDataRecoverer.java
+++ src/main/java/io/github/binaryfoo/decoders/SignedDataRecoverer.java
@@ -1,8 +1,19 @@
 package io.github.binaryfoo.decoders;
 
+import io.github.binaryfoo.crypto.CaPublicKey;
+import io.github.binaryfoo.tlv.ISOUtil;
+
 import java.math.BigInteger;
 
 public class SignedDataRecoverer {
+
+    public byte[] recover(String signed, CaPublicKey key) {
+        return recover(ISOUtil.hex2byte(signed), key.getExponent(), key.getModulus());
+    }
+
+    public byte[] recover(String signed, String exponent, String modulus) {
+        return recover(ISOUtil.hex2byte(signed), ISOUtil.hex2byte(exponent), ISOUtil.hex2byte(modulus));
+    }
 
     public byte[] recover(byte[] signed, byte[] exponent, byte[] modulus) {
         BigInteger biSigned = new BigInteger(1, signed);
