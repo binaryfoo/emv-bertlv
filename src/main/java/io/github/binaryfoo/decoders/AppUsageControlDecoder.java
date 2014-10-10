a97232c3b2346307af34f4ae2e81750f12b6e487
Will Currie <will@currie.id.au>
2014-10-10 21:26:33 +1100

    Use EmvBitStringDecoder for App Usage and Terminal Tx Qualifiers.

--- src/main/java/io/github/binaryfoo/decoders/AppUsageControlDecoder.java
+++ src/main/java/io/github/binaryfoo/decoders/AppUsageControlDecoder.java
@@ -1,16 +1,7 @@
 package io.github.binaryfoo.decoders;
 
-public class AppUsageControlDecoder extends FixedLengthDecoder {
+public class AppUsageControlDecoder extends EmvBitStringDecoder {
     public AppUsageControlDecoder() {
-        super(4, "8000", "Valid for domestic cash transactions",
-                 "4000", "Valid for international cash transactions",
-                "2000", "Valid for domestic goods",
-                "1000", "Valid for international goods",
-                "0800", "Valid for domestic services",
-                "0400", "Valid for international services",
-                "0200", "Valid at ATMs",
-                "0100", "Valid at terminals other than ATMs",
-                "0080", "Domestic cashback allowed",
-                "0040", "International cashback allowed");
+        super("fields/app-usage-control.txt", true);
     }
 }
