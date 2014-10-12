f34ef4a75189b21cf5f2b6e4d8a53433927cd786
Will Currie <will@currie.id.au>
2014-10-12 17:16:19 +1100

    Convert amex mobile cvm decoder to text file.

--- src/main/java/io/github/binaryfoo/decoders/MobileCVMDecoder.java
+++ src/main/java/io/github/binaryfoo/decoders/MobileCVMDecoder.java
@@ -1,48 +1,9 @@
 package io.github.binaryfoo.decoders;
 
-import io.github.binaryfoo.DecodedData;
-import io.github.binaryfoo.Decoder;
-
-import java.util.ArrayList;
-import java.util.List;
-
 // Table 12: Mobile CVM Results (Tag 9F71) Expresspay 3.0
-public class MobileCVMDecoder implements Decoder {
+public class MobileCVMDecoder extends EmvBitStringDecoder {
 
-    @Override
-    public List<DecodedData> decode(String input, int startIndexInBytes, DecodeSession decodeSession) {
-        List<DecodedData> decodedData = new ArrayList<DecodedData>();
-        int field = Integer.parseInt(input, 16);
-        if ((field & 0x010000) == 0x010000) {
-            decodedData.add(new DecodedData("", "Mobile CVM Performed", startIndexInBytes, startIndexInBytes + 1));
-        }
-        if ((field & 0x3F0000) == 0x3F0000) {
-            decodedData.add(new DecodedData("", "No CVM Performed", startIndexInBytes, startIndexInBytes + 1));
-        }
-        if ((field & 0x000300) == 0x0) {
-            decodedData.add(new DecodedData("", "Mobile CVM Not Required", startIndexInBytes + 1, startIndexInBytes + 2));
-        } else {
-            decodedData.add(new DecodedData("", "Terminal Required CVM", startIndexInBytes + 1, startIndexInBytes + 2));
-        }
-        if ((field & 0x000001) == 0x000001) {
-            decodedData.add(new DecodedData("", "Mobile CVM Failed", startIndexInBytes + 2, startIndexInBytes + 3));
-        }
-        if ((field & 0x000002) == 0x000002) {
-            decodedData.add(new DecodedData("", "Mobile CVM Successful", startIndexInBytes + 2, startIndexInBytes + 3));
-        }
-        if ((field & 0x000003) == 0x000003) {
-            decodedData.add(new DecodedData("", "Mobile CVM Blocked", startIndexInBytes + 2, startIndexInBytes + 3));
-        }
-        return decodedData;
-    }
-
-    @Override
-    public String validate(String input) {
-        return null;
-    }
-
-    @Override
-    public int getMaxLength() {
-        return 6;
+    public MobileCVMDecoder() {
+        super("fields/amex-mobile-cvm.txt", false);
     }
 }
