8adb5762b3d915ae574b0b8d81d1af9d91b6484f
Will Currie <will@currie.id.au>
2014-10-30 19:18:52 +1100

    Restore deleted tags.

--- src/main/java/io/github/binaryfoo/AmexTags.java
+++ src/main/java/io/github/binaryfoo/AmexTags.java
@@ -1,5 +1,13 @@
 package io.github.binaryfoo;
 
+import io.github.binaryfoo.tlv.Tag;
+
 public class AmexTags extends EmvTags {
     public static final TagMetaData METADATA = EmvTags.METADATA.join(TagMetaData.load("amex.yaml"));
+
+    public static final Tag TERMINAL_TX_CAPABILITIES = Tag.fromHex("9F6E");
+    public static final Tag EXPRESSPAY_TERMINAL_CAPABILITIES = Tag.fromHex("9F6D");
+    public static final Tag CARD_INTERFACE_CAPABILITIES = Tag.fromHex("9F70");
+    public static final Tag MOBILE_CVM_RESULTS = Tag.fromHex("9F71");
+    public static final Tag CHECK_CURRENCY_CODE = Tag.fromHex("9F66");
 }
--- src/main/java/io/github/binaryfoo/QVsdcTags.java
+++ src/main/java/io/github/binaryfoo/QVsdcTags.java
@@ -7,4 +7,5 @@ public class QVsdcTags extends EmvTags {
 
     public static final Tag TERMINAL_TX_QUALIFIERS = Tag.fromHex("9F66");
     public static final Tag CARD_TX_QUALIFIERS = Tag.fromHex("9F6C");
+    public static final Tag CARD_AUTH_RELATED_DATA = Tag.fromHex("9F69");
 }
