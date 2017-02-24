1ca8be420dd02bf449306229b55f069f3eff659b
Kikju <kiikju@gmail.com>
2017-02-24 13:23:31 +0100

    Removed named groups from patterns

--- src/main/java/io/github/binaryfoo/decoders/bit/EmvBitStringParser.kt
+++ src/main/java/io/github/binaryfoo/decoders/bit/EmvBitStringParser.kt
@@ -11,9 +11,9 @@ import kotlin.collections.map
 import kotlin.text.*
 
 object EmvBitStringParser {
-    val SINGLE_BIT_PATTERN = Pattern.compile("\\s*\\((?<byte>\\d+),(?<bit>\\d+)\\)=(?<value>\\d+)\\s*")
-    val NUMERIC_FIELD_PATTERN = Pattern.compile("\\s*\\((?<byte>\\d+),(?<firstBit>\\d+)-(?<lastBit>\\d+)\\)=INT\\s*")
-    val FULL_BYTE_FIELD_PATTERN = Pattern.compile("\\s*\\((?<byte>\\d+)\\)=0x(?<value>[0-9a-fA-F]{2})\\s*")
+    val SINGLE_BIT_PATTERN = Pattern.compile("\\s*\\((?\\d+),(?\\d+)\\)=(?\\d+)\\s*")
+    val NUMERIC_FIELD_PATTERN = Pattern.compile("\\s*\\((?\\d+),(?\\d+)-(?\\d+)\\)=INT\\s*")
+    val FULL_BYTE_FIELD_PATTERN = Pattern.compile("\\s*\\((?\\d+)\\)=0x(?[0-9a-fA-F]{2})\\s*")
 
     @JvmStatic fun parse(lines: List<String>): List<BitStringField> {
         fun usefulLine(line: String) = StringUtils.isNotBlank(line) && !line.startsWith("#")
